1bf805ee5390dddb778e3fcbe20d430397c3fc7c
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-11-04 21:41:56 +0000

    Update executives.vue

--- pages/executives.vue
+++ pages/executives.vue
@@ -8,36 +8,37 @@
             </div>
         </v-parallax>
         <v-container>
-            <v-row>
-                <v-col>
-                    <div class="d-flex">
-                        <v-expansion-panels v-for="chapter in chapters.data" :key="chapter.id" elevation="0">
-                            <v-expansion-panel :title="chapter.attributes.ChaterName">
-                                <v-expansion-panel-text v-for="executive in executives.data" :key="executive.id">
-                                    <!-- <v-container style="width: 11em;"> -->
-                                    <v-card color="#103778" height="150" width="150">
-                                        <!-- <v-img
+
+            <div class="d-flex">
+                <div v-for="chapter in chapters.data" :key="chapter.id">
+                    <v-expansion-panels elevation="0">
+                        <v-expansion-panel :title="chapter.attributes.ChaterName">
+                            <v-expansion-panel-text v-for="executive in executives.data" :key="executive.id">
+                                <!-- <v-container style="width: 11em;"> -->
+                                <v-card color="#103778" height="150" width="150">
+                                    <!-- <v-img
                                     v-if="'https://admin.gathstudents.com' + rep.attributes.ExecutivePic.data.attributes.url == true"
                                     height="200" cover src="/img/profile.png"></v-img> -->
-                                        <v-img height="200" round
-                                            :src="'https://admin.gathstudents.com' + executive.attributes.Picture.data.attributes.url"
-                                            cover style="border-radius: 50%;"></v-img>
+                                    <v-img height="200" round
+                                        :src="'https://admin.gathstudents.com' + executive.attributes.Picture.data.attributes.url"
+                                        cover style="border-radius: 50%;"></v-img>
 
-                                    </v-card><br>
-                                    <h5 class="text-start">{{ executive.attributes.Name }}</h5><br>
-                                    <h6 class="text-start">{{ executive.attributes.Title }}</h6>
+                                </v-card><br>
+                                <h5 class="text-start">{{ executive.attributes.Name }}</h5><br>
+                                <h6 class="text-start">{{ executive.attributes.Title }}</h6>
 
-                                    <!-- </v-container> -->
-                                </v-expansion-panel-text>
+                                <!-- </v-container> -->
+                            </v-expansion-panel-text>
 
-                            </v-expansion-panel>
+                        </v-expansion-panel>
 
-                            <v-spacer></v-spacer>
-                            <!-- <h3>{{ executive.attributes.Name }}</h3> -->
-                        </v-expansion-panels>
-                    </div>
-                </v-col>
-            </v-row>
+                        <v-spacer></v-spacer>
+                        <!-- <h3>{{ executive.attributes.Name }}</h3> -->
+                    </v-expansion-panels>
+                </div>
+
+            </div>
+
         </v-container>
     </div>
 </template>
