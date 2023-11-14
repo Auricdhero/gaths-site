e0395b92e1a342c78c0d87399a72afa0e25433c3
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-11-14 20:09:14 +0000

    simple updates on footer

--- components/theFooter.vue
+++ components/theFooter.vue
@@ -11,55 +11,55 @@
                             <v-col cols="auto" lg="3" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Membership</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Membership Benefits</li>
-                                    <li>Registration</li>
-                                    <li>Open a Chapter</li>
-                                    <li>Become a Member</li>
+                                    <li><nuxt-link to=""> Membership Benefits</nuxt-link></li>
+                                    <li><nuxt-link to="">Registration</nuxt-link> </li>
+                                    <li><nuxt-link to="">Partner With Us</nuxt-link> </li>
+                                    <li><nuxt-link to="">Become a Member</nuxt-link> </li>
                                 </ul>
                             </v-col>
                             <v-col cols="auto" lg="3" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Advocacy</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Policy Agenda</li>
-                                    <li>Get Involved</li>
-                                    <li>Legacy Advocacy</li>
-                                    <li>Commitment to SDG's</li>
+                                    <li><nuxt-link to="">Policy Agenda</nuxt-link> </li>
+                                    <li><nuxt-link to="">Get Involved</nuxt-link> </li>
+                                    <li><nuxt-link to="">Legacy Advocacy</nuxt-link> </li>
+                                    <li><nuxt-link to="">Commitment to SDG's</nuxt-link> </li>
                                 </ul>
                             </v-col>
                             <v-col cols="auto" lg="3" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Events</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Event Calandar</li>
-                                    <li>Out Reaches</li>
-                                    <li>Open a Chapter</li>
-                                    <li>Become a Member</li>
+                                    <li><nuxt-link to="/events/"></nuxt-link> Event Calandar</li>
+                                    <li><nuxt-link to="">Out Reaches</nuxt-link> </li>
+                                    <li><nuxt-link to="">Partner With Us</nuxt-link> </li>
+                                    <li><nuxt-link to="">Become a Member</nuxt-link> </li>
                                 </ul>
                             </v-col>
                             <v-col cols="auto" lg="3" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Organization</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Event Calandar</li>
-                                    <li>Out Reaches</li>
-                                    <li>Open a Chapter</li>
-                                    <li>Become a Member</li>
+                                    <li><nuxt-link to="">Event Calandar</nuxt-link> </li>
+                                    <li><nuxt-link to="">Out Reaches</nuxt-link> </li>
+                                    <li><nuxt-link to="">Partner With Us</nuxt-link> </li>
+                                    <li><nuxt-link to="">Become a Member</nuxt-link> </li>
                                 </ul>
                             </v-col>
                             <v-col cols="auto" lg="5" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Resources</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Resource Library</li>
-                                    <li>Marketplace</li>
-                                    <li>Learning Center</li>
+                                    <li><nuxt-link to="">Resource Library</nuxt-link> </li>
+                                    <li><nuxt-link to="">Marketplace</nuxt-link> </li>
+                                    <li><nuxt-link to="">Learning Center</nuxt-link></li>
                                     <!-- <li>Become a Member</li> -->
                                 </ul>
                             </v-col>
                             <v-col cols="auto" lg="3" sm="12" md="6">
                                 <h5 class="text-left" style="color: rgb(254, 9, 9);">Legal</h5>
                                 <ul class="text-left ml-n8">
-                                    <li>Terms & Conditions</li>
-                                    <li>Privacy Policy</li>
-                                    <li>Cookie Policy</li>
-                                    <li>Website Accessibility Statement</li>
+                                    <li><nuxt-link to="">Terms & Conditions</nuxt-link></li>
+                                    <li><nuxt-link to="">Privacy Policy</nuxt-link></li>
+                                    <li><nuxt-link to="">Cookie Policy</nuxt-link></li>
+                                    <li><nuxt-link to="">Website Accessibility Statement</nuxt-link></li>
                                 </ul>
                             </v-col>
                         </v-row>
@@ -71,19 +71,11 @@
                             opportunities to
                             learn from industry experts, network with. network with professionals, and make a t industry.
                         </div>
-                        <!-- <v-form class="mt-7">
-
-                            <v-row no-gutters>
-                                <v-col cols="auto" lg="8" md="8" sm="8">
-                                    <v-text-field variant="outlined"
-                                        placeholder="Enter your email address to subscribe"></v-text-field>
-                                </v-col>
-                                <v-col cols="auto" lg="3" md="3" sm="3">
-                                    <v-btn style="height: 55px;">Subscribe</v-btn>
-                                </v-col>
-                            </v-row>
-
-                        </v-form> -->
+                        <v-form class="mt-7">
+                            <v-text-field variant="outlined"
+                                placeholder="Enter your email address to subscribe"></v-text-field>
+                            <v-btn style="height: 55px;">Subscribe</v-btn>
+                        </v-form>
                     </v-col>
                 </v-row>
 
